0fdfcf53fb69e471302a1ef4a97832dea23309de
kailey clifford <kailey.clifford22@sitechhs.com>
2021-05-20 01:37:46 -0400

    followed videos + postman

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+const key = "5abf5331108fe97305b5c8a3b501789e";
+
+const query = async function () {
+  try {
+    const response = await fetch(
+      "https://api.musixmatch.com/ws/1.1/track.get?format=jsonp&callback=callback&track_id=2&apikey=https://developer.musixmatch.com/activate/aa87805981fb1a20b593ad2c3bb8dc0d4cbf2e62"
+    );
+    const data = await response.json();
+  } catch (error) {
+    console.log(error);
+    alert("a problem occured");
+  }
+};
